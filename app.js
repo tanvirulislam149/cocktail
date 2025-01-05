e2e4d98579049e649b5d61a24aa1ad17e4c6db28
tanvir <tanvirulislam149@gmail.com>
2025-01-06 00:04:10 +0600

    task-1 done

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("checking");
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=ma")
+  .then((res) => res.json())
+  .then((data) => {
+    displayData(data.drinks);
+  });
+
+const displayData = (allData) => {
+  const show_data = document.getElementById("show_data");
+  console.log(allData);
+  allData.forEach((element) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card h-100" style="width: 18rem;">
+        <img src="${element.strDrinkThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Name: ${element.strDrink}</h5>
+            <h6 class="card-title">Category: ${element.strCategory}</h6>
+            <p title="${
+              element.strInstructions
+            }" class="card-text">Instructions: ${element.strInstructions.substring(
+      0,
+      15
+    )}${element.strInstructions.length > 15 ? "..." : ""}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    `;
+    show_data.appendChild(div);
+  });
+};
+
+document.getElementById("search_btn").addEventListener("click", (e) => {
+  const text = document.getElementById("search_box").value;
+  console.log(text);
+});
